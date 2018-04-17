d687c76f26b87544941f463b3d257552a049bcf4
Jean-Christophe Lavocat <jice@lavocat.name>
2018-04-17 08:45:46 +0200

    Added first landing page

---
+++ static/landing/soin_1/js/script.js
@@ -0,0 +1,251 @@
+/**
+ * @function      Include
+ * @description   Includes an external scripts to the page
+ * @param         {string} scriptUrl
+ */
+function include(scriptUrl) {
+    document.write('<script src="' + scriptUrl + '"></script>');
+}
+
+/**
+ * @function      Include
+ * @description   Lazy script initialization
+ */
+function lazyInit(element, func) {
+    var $win = jQuery(window),
+        wh = $win.height();
+
+
+    $win.on('load scroll', function () {
+        var st = $(this).scrollTop();
+        if (!element.hasClass('lazy-loaded')) {
+            var et = element.offset().top,
+                eb = element.offset().top + element.outerHeight();
+            if (st + wh > et - 100 && st < eb + 100) {
+                func.call();
+                element.addClass('lazy-loaded'); 
+            }
+        }
+    });
+}
+
+/**
+ * @function      isIE
+ * @description   checks if browser is an IE
+ * @returns       {number} IE Version
+ */
+function isIE() {
+    var myNav = navigator.userAgent.toLowerCase();
+    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+};
+
+
+/**
+ * @module       Copyright
+ * @description  Evaluates the copyright year
+ */
+;
+(function ($) {
+    $(document).ready(function () {
+        $("#copyright-year").text((new Date).getFullYear());
+    });
+})(jQuery);
+
+
+/**
+ * @module       IE Fall&Polyfill
+ * @description  Adds some loosing functionality to old IE browsers
+ */
+;
+(function ($) {
+    if (isIE() && isIE() < 11) {
+        $('html').addClass('lt-ie11');
+        $(document).ready(function () {
+            PointerEventsPolyfill.initialize({});
+        });
+    }
+
+    if (isIE() && isIE() < 10) {
+        $('html').addClass('lt-ie10');
+    }
+})(jQuery);
+
+
+/**
+ * @module       WOW Animation
+ * @description  Enables scroll animation on the page
+ */
+;
+(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop') && o.hasClass("wow-animation") && $(".wow").length) {
+        $(document).ready(function () {
+            new WOW().init();
+        });
+    }
+})(jQuery);
+
+
+/**
+ * @module       ToTop
+ * @description  Enables ToTop Plugin
+ */
+;
+(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+
+        $(document).ready(function () {
+            $().UItoTop({
+                easingType: 'easeOutQuart',
+                containerClass: 'ui-to-top fa fa-chevron-up'
+            });
+        });
+    }
+})(jQuery);
+
+/**
+ * @module       Responsive Tabs
+ * @description  Enables Easy Responsive Tabs Plugin
+ */
+;
+(function ($) {
+    var o = $('.responsive-tabs');
+    if (o.length > 0) {
+        $(document).ready(function () {
+            o.each(function () {
+                var $this = $(this);
+                $this.easyResponsiveTabs({
+                    type: $this.attr("data-type") === "accordion" ? "accordion" : "default"
+                });
+            })
+        });
+    }
+})(jQuery);
+
+/**
+ * @module       RD Mailform
+ * @description  Enables RD Mailform Plugin
+ */
+;
+(function ($) {
+    var o = $('.rd-mailform');
+    if (o.length > 0) {
+        $(document).ready(function () {
+            var o = $('.rd-mailform');
+
+            if (o.length) {
+                o.rdMailForm({
+                    validator: {
+                        'constraints': {
+                            '@LettersOnly': {
+                                message: 'Please use only letters.'
+                            },
+                            '@NumbersOnly': {
+                                message: 'Please use only numbers.'
+                            },
+                            '@NotEmpty': {
+                                message: 'This field should not be empty.'
+                            },
+                            '@Email': {
+                                message: 'Enter valid e-mail address.'
+                            },
+                            '@Phone': {
+                                message: 'Enter valid phone number.'
+                            },
+                            '@Date': {
+                                message: 'Use MM/DD/YYYY format.'
+                            },
+                            '@SelectRequired': {
+                                message: 'Please choose an option.'
+                            }
+                        }
+                    }
+                }, {
+                    'MF000': 'Sent',
+                    'MF001': 'Recipients are not set.',
+                    'MF002': 'Form will not work locally.',
+                    'MF003': 'Please define email field in your form.',
+                    'MF004': 'Please define the type of your form.',
+                    'MF254': 'Something went wrong with PHPMailer.',
+                    'MF255': 'There was an error submitting the form.'
+                });
+            }
+        });
+    }
+})(jQuery);
+
+/**
+ * @module       RD Google Map
+ * @description  Enables RD Google Map Plugin
+ */
+
+;
+(function ($) {
+    var o = $('#google-map');
+
+    if (o.length) {
+        include('//maps.google.com/maps/api/js');
+        $(document).ready(function () {
+            var head = document.getElementsByTagName('head')[0],
+                insertBefore = head.insertBefore;
+
+            head.insertBefore = function (newElement, referenceElement) {
+                if (newElement.href && newElement.href.indexOf('//fonts.googleapis.com/css?family=Roboto') != -1 || newElement.innerHTML.indexOf('gm-style') != -1) {
+                    return;
+                }
+                insertBefore.call(head, newElement, referenceElement);
+            };
+
+            lazyInit(o, function () {
+                o.googleMap({
+                    styles: [{"featureType":"administrative.locality","elementType":"all","stylers":[{"hue":"#2c2e33"},{"saturation":7},{"lightness":19},{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#e9ebed"},{"saturation":-90},{"lightness":-8},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":10},{"lightness":69},{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]}]
+                });
+            });
+        });
+    }
+})(jQuery);
+
+/**
+ * @module       Magnific Popup
+ * @description  Enables Magnific Popup Plugin
+ */
+;
+(function ($) {
+
+    var o = $('[data-lightbox]').not('[data-lightbox="gallery"] [data-lightbox]'),
+        g = $('[data-lightbox^="gallery"]');
+
+    if (o.length > 0 || g.length > 0) {
+
+        $(document).ready(function () {
+            if (o.length) {
+                o.each(function () {
+                    var $this = $(this);
+                    $this.magnificPopup({
+                        type: $this.attr("data-lightbox")
+                    });
+                })
+            }
+
+            if (g.length) {
+                g.each(function () {
+                    var $gallery = $(this);
+                    $gallery
+                        .find('[data-lightbox]').each(function () {
+                            var $item = $(this);
+                            $item.addClass("mfp-" + $item.attr("data-lightbox"));
+                        })
+                        .end()
+                        .magnificPopup({
+                            delegate: '[data-lightbox]',
+                            type: "image",
+                            gallery: {
+                                enabled: true
+                            }
+                        });
+                })
+            }
+        });
+    }
+})(jQuery);
